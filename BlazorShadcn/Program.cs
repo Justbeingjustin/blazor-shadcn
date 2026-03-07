7fc274883016d309a6c18bceb3b56aab89530ef3
Justin Gerber <JustinShawnGerber@gmail.com>
2026-03-07 13:16:19 -0800

    Update Program.cs

--- BlazorShadcn/Program.cs
+++ BlazorShadcn/Program.cs
@@ -1,11 +1,25 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 using BlazorShadcn.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor |
+        ForwardedHeaders.XForwardedProto |
+        ForwardedHeaders.XForwardedHost;
+
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 if (!app.Environment.IsDevelopment())
 {
