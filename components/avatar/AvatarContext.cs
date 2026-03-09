541341bda2250cc49834659087203026b7c5ccab
Justin Gerber <JustinShawnGerber@gmail.com>
2026-03-08 19:47:54 -0700

    changes

---
+++ components/avatar/AvatarContext.cs
@@ -0,0 +1,24 @@
+namespace ShadcnBlazor.Components.UI;
+
+internal sealed class AvatarContext
+{
+    private Func<Task>? _stateChanged;
+
+    public bool IsImageLoaded { get; private set; }
+
+    public void Configure(Func<Task> stateChanged)
+    {
+        _stateChanged = stateChanged;
+    }
+
+    public Task SetImageLoadedAsync(bool loaded)
+    {
+        if (IsImageLoaded == loaded)
+        {
+            return Task.CompletedTask;
+        }
+
+        IsImageLoaded = loaded;
+        return _stateChanged is null ? Task.CompletedTask : _stateChanged();
+    }
+}
