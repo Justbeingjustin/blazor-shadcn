4a12c4261d6ecd14024d2325a42b56036842bdda
Justin Gerber <JustinShawnGerber@gmail.com>
2026-03-10 14:45:01 -0700

    Add tooltip

--- BlazorShadcn/Components/UI/TooltipContext.cs
+++ BlazorShadcn/Components/UI/TooltipContext.cs
@@ -2,6 +2,7 @@ namespace BlazorShadcn.Components.UI;
 
 public sealed class TooltipContext
 {
-    public required bool Open { get; init; }
+    public required bool Open { get; set; }
+    public required int DelayDuration { get; init; }
     public required Func<bool, Task> SetOpenAsync { get; init; }
 }
