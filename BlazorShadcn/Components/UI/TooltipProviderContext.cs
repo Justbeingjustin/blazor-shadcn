90dfaf28157fbf0e390f64c5d8ea2499e50cbbd6
Justin Gerber <JustinShawnGerber@gmail.com>
2026-03-10 14:53:37 -0700

    changes

---
+++ BlazorShadcn/Components/UI/TooltipProviderContext.cs
@@ -0,0 +1,6 @@
+namespace BlazorShadcn.Components.UI;
+
+public sealed class TooltipProviderContext
+{
+    public required int DelayDuration { get; init; }
+}
---
+++ components/tooltip/TooltipProviderContext.cs
@@ -0,0 +1,6 @@
+@namespace ShadcnBlazor.Components.UI
+
+public sealed class TooltipProviderContext
+{
+    public required int DelayDuration { get; init; }
+}
